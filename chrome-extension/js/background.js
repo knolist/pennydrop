318cf6cb698a2bde5eb0a047e035f387354d23c2
Evan Mays <me@evanmays.com>
2020-03-23 16:12:33 -0400

    Added google signin

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -1,9 +1,49 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB5EjYw_uhjZjgZYxsK_JongqAPkupo270",
+  authDomain: "penny-flop.firebaseapp.com",
+  databaseURL: "https://penny-flop.firebaseio.com",
+  projectId: "penny-flop",
+  storageBucket: "penny-flop.appspot.com",
+  messagingSenderId: "230720099647",
+  appId: "1:230720099647:web:29fe3da92922d6b81b6f8a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 window.urls = {};
-chrome.runtime.onMessage.addListener(function(request) {
-    if (request.url !== null) {
-        window.urls[request.url] = request.url;
+chrome.runtime.onMessage.addListener(function(message, _sender, _sendResponse) {
+    if (message.type == "knolistPopupLoaded") {
+        //Get current authentication state and tell popup
+        let user = firebase.auth().currentUser;
+        if (user) {
+          _sendResponse({type: "loggedIn"});
+        } else {
+          _sendResponse({type: "loggedOut"});
+        };
+    } else if (message.type == "attemptLogin") {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
     }
-    if (request.command === "reset") {
+    else if (message.url !== null) {
+        window.urls[message.url] = message.url;
+    }
+    else if (message.command === "reset") {
         window.urls = {};
     }
 });
--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -1,14 +1,18 @@
 resetButtonClicked = () => {
     chrome.runtime.sendMessage({command: "reset"});
-    chrome.tabs.reload();
+    //chrome.tabs.reload();
 }
 
 viewFullWebsiteButtonClicked = () => {
     chrome.tabs.create({url: 'http://nolist.com'})
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+tryToLogin = () => {
+    chrome.runtime.sendMessage({type: "attemptLogin"});
+    //chrome.tabs.reload();
+}
 
+createClickListeners = () => {
     // Add the urls to the page
     const bg = chrome.extension.getBackgroundPage();
     Object.keys(bg.urls).forEach(function (url) {
@@ -16,8 +20,32 @@ document.addEventListener('DOMContentLoaded', function () {
         div.textContent = `${url}`;
         document.body.appendChild(div)
     });
-
+    chrome.runtime.sendMessage({type: "knolistPopupLoaded"}, function (response) {
+        if (response.type == "loggedIn") {
+            document.querySelector("#loginSection").style.display = "none";
+            document.querySelector("#alreadyLoggedInSection").style.display = "block";
+        } else if (response.type == "loggedOut") {
+            document.querySelector("#loginSection").style.display = "block";
+            document.querySelector("#alreadyLoggedInSection").style.display = "none";
+        };
+    });
     // Button Listeners
     document.getElementById('reset-button').addEventListener('click', resetButtonClicked, false);
+    document.getElementById('login-button').addEventListener('click', tryToLogin, false);
     document.getElementById('full-website-button').addEventListener('click', viewFullWebsiteButtonClicked, false);
-}, false);
+}
+
+document.addEventListener('DOMContentLoaded', createClickListeners, false);
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB5EjYw_uhjZjgZYxsK_JongqAPkupo270",
+  authDomain: "penny-flop.firebaseapp.com",
+  databaseURL: "https://penny-flop.firebaseio.com",
+  projectId: "penny-flop",
+  storageBucket: "penny-flop.appspot.com",
+  messagingSenderId: "230720099647",
+  appId: "1:230720099647:web:29fe3da92922d6b81b6f8a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
