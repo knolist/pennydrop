f99e013c376533d5d0057980fb9dfab80b06ab39
Evan Mays <me@evanmays.com>
2020-03-23 19:52:48 -0400

    Sites visited are now in firebase

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -11,13 +11,19 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Get a reference to the database service
+var database = firebase.database();
+
+addWebpageToDatabase = (userId, url) => {
+    firebase.database().ref('sitesVisited/' + userId).push(url);
+}
+
 window.urls = {};
 chrome.runtime.onMessage.addListener(function(message, _sender, _sendResponse) {
+    let user = firebase.auth().currentUser;
     if (message.type == "knolistPopupLoaded") {
-        //Get current authentication state and tell popup
-        let user = firebase.auth().currentUser;
         if (user) {
-          _sendResponse({type: "loggedIn"});
+          _sendResponse({type: "loggedIn", uid: user.uid});
         } else {
           _sendResponse({type: "loggedOut"});
         };
@@ -28,7 +34,7 @@ chrome.runtime.onMessage.addListener(function(message, _sender, _sendResponse) {
           var token = result.credential.accessToken;
           // The signed-in user info.
           var user = result.user;
-          // ...
+          _sendResponse({uid: user.uid});
         }).catch(function(error) {
           // Handle Errors here.
           var errorCode = error.code;
@@ -40,8 +46,8 @@ chrome.runtime.onMessage.addListener(function(message, _sender, _sendResponse) {
           // ...
         });
     }
-    else if (message.url !== null) {
-        window.urls[message.url] = message.url;
+    else if (message.url !== null && user) {
+        addWebpageToDatabase(user.uid, message.url)
     }
     else if (message.command === "reset") {
         window.urls = {};
--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -8,22 +8,34 @@ viewFullWebsiteButtonClicked = () => {
 }
 
 tryToLogin = () => {
-    chrome.runtime.sendMessage({type: "attemptLogin"});
-    //chrome.tabs.reload();
+    chrome.runtime.sendMessage({type: "attemptLogin"}, function(response) {
+        createListenerToListSitesVisited(response.uid)
+    });
+}
+
+createListenerToListSitesVisited = (userId) => {
+    console.log(firebase.database())
+    var visitedSitesDatabase = firebase.database().ref('sitesVisited/' + userId);
+    console.log(visitedSitesDatabase)
+    visitedSitesDatabase.on('value', function (snapshot) {
+      console.log(snapshot.val())
+      websites = snapshot.val();
+      websites = Object.values(websites);
+      websites.forEach((url) => {
+        const div = document.createElement('div');
+        div.textContent = `${url}`;
+        document.body.appendChild(div)
+      });
+    });
 }
 
 createClickListeners = () => {
     // Add the urls to the page
-    const bg = chrome.extension.getBackgroundPage();
-    Object.keys(bg.urls).forEach(function (url) {
-        const div = document.createElement('div');
-        div.textContent = `${url}`;
-        document.body.appendChild(div)
-    });
     chrome.runtime.sendMessage({type: "knolistPopupLoaded"}, function (response) {
         if (response.type == "loggedIn") {
             document.querySelector("#loginSection").style.display = "none";
             document.querySelector("#alreadyLoggedInSection").style.display = "block";
+            createListenerToListSitesVisited(response.uid)
         } else if (response.type == "loggedOut") {
             document.querySelector("#loginSection").style.display = "block";
             document.querySelector("#alreadyLoggedInSection").style.display = "none";
@@ -49,3 +61,6 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+var database = firebase.database();
